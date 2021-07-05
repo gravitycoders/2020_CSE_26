dc49035b350cf57fac516177afd2b3af265bbef2
gravitycoders <83114746+gravitycoders@users.noreply.github.com>
2021-07-05 13:59:33 +0530

    Add files via upload

--- GUI/pydb.py
+++ GUI/pydb.py
@@ -3,12 +3,14 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 import stream
-
+from datetime import date
+from datetime import datetime
 # initializations 
 cred = credentials.Certificate('face-recognition-key.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
- 
+
+
  
  
 #adding first data
@@ -19,6 +21,8 @@ def init_db():
 
 def init_company(email):
     global company
+    global deptnames,depts,dept
+    deptnames=[]
     print(email)
     query = collectiondb.where('email', '==', email ).get()
     for q in query:
@@ -27,20 +31,57 @@ def init_company(email):
     company=collectiondb.document(cname)
     global employee,face_data,all_encodings
     employee=company.collection('employees')
+    dept=company.collection('department')
+    depts=dept.get()
+    deptnames=printdept(depts)
     face_data=company.collection('face_data')
     all_encodings = face_data.stream()
     org_face_data(all_encodings)
+    generateday()
 
+def generateday():
+    global attendance,today
+    attendance = company.collection('attendance')
+    currdate=date.today()
+    today=attendance.document(str(currdate))
+    print('Today database created')
 
-def add_employee(name,role):
+def printdept(depts):
+    deptnamestemp=[]
+    for i in depts:
+        tempdict=i.to_dict()
+        #print(tempdict)
+        deptnamestemp.append(tempdict['name'])
+    return deptnamestemp
+
+def retdeptlist():
+    #print(deptnames)
+    return deptnames
+
+def generatempid(deptname):
+    deptdict=dept.document(deptname)
+    deptdictemp=deptdict.get().to_dict()
+    depttot=int(deptdictemp['total'])+1
+    deptcode=deptdictemp['code']
+    empid=deptcode+'21'+str(depttot)
+    #print(empid,type(deptdictemp))
+    deptdictemp['total']=depttot
+    #print(deptdictemp)
+    deptdict.set(deptdictemp)
+    return empid
+
+def add_employee(name,role,deptname):
     global employee
     new_emp={
         'name':name,
-        'role':role
+        'role':role,
+        'department':deptname
     }
-    doc=employee.document()
+    empid=generatempid(deptname)
+    doc=employee.document(empid)
     doc.set(new_emp)
-    return doc.id,name
+    return empid
+
 
 def add_face(uid,jsdoc):
     global face_data    
@@ -57,5 +98,12 @@ def org_face_data(all_encodings):
        facestr.append(currdoc['face'])
     stream.get_data(names,facestr)
     
+def todaylist(currempid,currtime):
+    today.set({currempid:currtime }, merge=True)
+    print('updated attendance')
+
+def get_today_list_db():
+    tlist=today.get()
+    return tlist.to_dict()
 
 print('success')
